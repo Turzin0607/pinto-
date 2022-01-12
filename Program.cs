2c706b268dd8aa3104052097430008042c14398e
Turzin0607 <97605782+Turzin0607@users.noreply.github.com>
2022-01-12 15:01:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(string.Format("Question 1"));
+Console.WriteLine(string.Format("which number divided by 2 is equal 5 ?"));
+int variavel2 = 2;
+double variavel3 = double.Parse(Console.ReadLine());
+Console.WriteLine(variavel3 / variavel2);
+if (variavel3 == 10)
+{
+    Console.WriteLine("ta certo" );
+}
+else
+{
+    Console.WriteLine("tu e burro pra nao saber isso");
+}
+Console.WriteLine(string.Format("Question 2"));
+Console.WriteLine(string.Format("which number multiplied by 5 is equal to 80?"));
+int variavel4 = 5;
+double variavel5 = double.Parse(Console.ReadLine());
+Console.WriteLine(variavel5*variavel4);    
+if (variavel5 == 16)
+{
+    Console.WriteLine("ta certo");
+}
+else
+{
+    Console.WriteLine("tu e burro pra nao saber isso");
+}
